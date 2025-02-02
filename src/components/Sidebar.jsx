7bcbcfb39e27a86d242f5a9a6930dc220aac2c66
pooYAYooq <pooyayooq@gmail.com>
2025-02-02 13:19:01 +1100

    feat: add logo and implement Sidebar component with navigation and social links

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,43 @@
+//IMPORTS
+import { FaTimes } from 'react-icons/fa';
+import { useGlobalContext } from '../context';
+import { links, social } from '../data';
+import logo from '../assets/logo.svg';
+
 const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+
   return (
-    <div>Sidebar</div>
-  )
-}
-export default Sidebar
+    <aside className="sidebar">
+      <div className="sidebar-header">
+        <img src={logo} alt="logo" className="logo" />
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          const { id, text, url, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {text} {icon}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="social-links">
+        {social.map((socialLink) => {
+          const { id, url, icon } = socialLink;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
+};
+export default Sidebar;
